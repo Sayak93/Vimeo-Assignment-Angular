d700788fc0f199be77cb6a1bc5311b881691f6c4
sayak <chatterjee.sayak93@gmail.com>
2021-05-29 11:43:32 +0530

    radio buttons and components added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sayak-chatterjee-may292021';
+export class AppComponent implements OnInit{
+  public title: string;
+  public buttonColor: string;
+  public colorChangerForm: FormGroup;
+  constructor(public fb: FormBuilder) { }
+
+  ngOnInit() {
+    /*########### Form ###########*/
+    this.colorChangerForm = this.fb.group({
+      color: ['blue', [Validators.required]]
+    });
+
+    this.title = 'sayak-chatterjee-may292021';
+    this.buttonColor = 'blue';
+  }
+
+  // Getter method to access form control
+  get myForm() {
+    return this.colorChangerForm.get('gender');
+  }
+
+  //on selecting a different color
+  changeColor(){
+    if(this.colorChangerForm.valid){
+      this.buttonColor = this.colorChangerForm.value.color;
+    }
+    console.log(this.buttonColor);
+    
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
